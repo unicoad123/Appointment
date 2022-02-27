19a24f437ff7d2585160a821be58db876a7f2a29
unicoad123 <41484199+unicoad123@users.noreply.github.com>
2022-02-27 12:11:54 +0530

    Update main.js

--- main.js
+++ main.js
@@ -6,3 +6,21 @@ let usermail=document.getElementById('email');
 localStorage.setItem('name',username.value);
 localStorage.setItem('mail',usermail.value);
 }
+
+------------------------------usingobject---------------------------
+   function f1()
+{
+    
+    let obj={
+
+        username:document.getElementById('name').value,
+        usermail:document.getElementById('email').value
+
+    };
+    let obj_serialized=JSON.stringify(obj);
+    localStorage.setItem(obj,obj_serialized);
+
+    var retrievedObject = localStorage.getItem('obj');
+    console.log('retrieved data Object: ', JSON.parse(retrievedObject));
+
+}
